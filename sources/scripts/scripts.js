0a955a09d2478bf64334c181afe1473bed91a6a7
Lueh <jeanpaulluehmann@web.de>
2017-12-05 18:14:21 +0100

    Lueh
    addet functionality for page swap and some styles

--- sources/scripts/scripts.js
+++ sources/scripts/scripts.js
@@ -4,21 +4,38 @@ $(".nav__link").click(function (event) {
 	event.preventDefault();
 	var $main = $(".main")
 	var isSplit = $main.hasClass("main--split");
-	var target = $(event.currentTarget).attr("href");
+	var $target = $(event.currentTarget);
+	var targetid = $target.attr("href");
+	var activeclass = "nav__link--active";
+	var hiddenClass = "hidden";
+	var logoTextSelector = ".logo__wrapper .logo__text";
+	var logoTextSelectorWithModifier = logoTextSelector + "--split";
 
 	if(isSplit) {
 		
 		if (klickedTarget == event.currentTarget) {
-			$main.toggleClass("main--split", false);
+			$main.removeClass("main--split");
+			$(".nav__link").removeClass(activeclass);
+			$(logoTextSelector).removeClass(hiddenClass);
+			$(logoTextSelectorWithModifier).addClass(hiddenClass).html();
+		} else {
+			$(".nav__link").removeClass(activeclass);
+			$target.addClass(activeclass);
+			$(logoTextSelectorWithModifier).html($target.data("title"));
 		}
-
-		$(".section").addClass("hidden");
-		$(target).removeClass("hidden");
-		///Mach was
-		
+			
 	} else {
-		$main.toggleClass("main--split", true);
+		$main.addClass("main--split");
+		$target.addClass(activeclass);
+		$(logoTextSelector).addClass(hiddenClass);
+		$(logoTextSelectorWithModifier).html($target.data("title")).removeClass(hiddenClass);
 	}
+
+	
+	
+	
+	$(".section").addClass(hiddenClass);
+	$(targetid).removeClass(hiddenClass);
 
 	klickedTarget = event.currentTarget;
 });
